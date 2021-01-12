ef2fd5988689552b2b754d678e0baf0196ab0a73
AhmedMahmoud55 <ahmed.nah33@gmail.com>
2021-01-12 02:14:36 +0200

    first commit

--- pset2/Caesar/main.c
+++ pset2/Caesar/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
 #include <stdlib.h>
 
-int main()
+
+int main(int argc, string argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+    // check there is only 1 arguments and it is a number else returns instructions
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+         
+    }
+    for (int i = 0, n = strlen(argv[1]) ; i < n ; i++)
+    {
+        if (!(isdigit(argv[1][i])))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+            
+        }
+    }
+    // The atoi function converts a string to an integer
+    int k = atoi(argv[1]);
+    int ci;
+    string s = get_string("plaintext: "); //get text
+    printf("ciphertext: "); // print out cipher
+    for (int j = 0, l = strlen(s); j < l; j++)
+    {
+        if (isalpha(s[j])) //check if text not char
+        {
+            //check if it is an uppercase letter
+            if (isupper(s[j]))
+            {
+                s[j] -= 65;
+                ci = (s[j] + k) % 26;
+                printf("%c", ci + 65);
+            }
+            //check if it is an lower letter
+            else if (islower(s[j]))
+            {
+                s[j] -= 97;
+                ci = (s[j] + k) % 26;
+                printf("%c", ci + 97); 
+            }
+             
+        }
+        else
+        {
+            printf("%c", s[j]);
+        }
+         
+    }
+    printf("\n");
+
 }
--- pset2/Readability/main.c
+++ pset2/Readability/main.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <string.h>
-#include<cs50.h>
-#include<ctype.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
 
 int main()
 {
 
     string s = get_string("Text: ");
 
-    int letters =0,words=0,sentences=0;
+    int letters = 0, words = 0, sentences = 0;
 
-    for(int i =0 ;i<strlen(s);i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(islower (s[i])|| isupper(s[i]))
+        if (islower(s[i]) || isupper(s[i]))
         {
             letters++;
         }
-        else if(s[i]==32)
+        else if (s[i] == 32)
         {
             words++;
         }
-        else if (s[i]==33||s[i]==46||s[i]==63)
+        else if (s[i] == 33 || s[i] == 46 || s[i] == 63)
         {
             sentences++;
         }
     }
-    words+=1;
-    float index = (0.0588 *(float) letters / words *100) - (0.296 *(float) sentences / words *100) - 15.8;
+    words += 1;
+    float index = (0.0588 * (float) letters / words * 100) - (0.296 * (float) sentences / words * 100) - 15.8;
     float grade = index;
 
     //printf("letters : %i\nwords : %i\nsentences : %i\n",letters,words,sentences);
-    if(grade<1)
+    if (grade < 1)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
-    else if (grade>=16)
+    else if (grade >= 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade : %i", (int) round(grade));
+        printf("Grade %i\n", (int) round(grade));
     }
 
 }
